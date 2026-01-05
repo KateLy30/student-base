5fd245d73f6cdb90d239db7e25946582a86dbb7c
KateLy30 <kateshytova2006@gmail.com>
2026-01-05 13:32:48 +0300

    Initial commit

---
+++ StudentBase.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentBase.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ StudentBase.MAUI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "StudentBase.MAUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "StudentBase.MAUI.Pages")]

62ab5a58b104de92574df36c574ee198dafcb405
KateLy30 <kateshytova2006@gmail.com>
2026-01-05 13:26:55 +0300

    Clean repository

--- StudentBase/src/StudentBase.Maui/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "StudentBase.Maui")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "StudentBase.Maui.Pages")]
